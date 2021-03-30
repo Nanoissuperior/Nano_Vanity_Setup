6cbf55d75e77a4e67bf1e0543ddc67c474ab14fb
Nanoissuperior <50946350+Nanoissuperior@users.noreply.github.com>
2021-03-30 18:21:12 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ apt update -y
 apt install ocl-icd-opencl-dev -y
 apt install curl -y
 apt install build-essential -y
+clear
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 cargo install nano-vanity --features gpu
